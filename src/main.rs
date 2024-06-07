c2782555041cc122c6b8552bbf317b0428cb0ecb
ralrafi12@gmail.com <ralrafi12@gmail.com>
2024-06-07 11:10:05 +0700

    Print the option usage if used

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,23 @@ fn main() {
 
     args.next();
     
-    let args_1 = args.next().unwrap();
-    println!("{args_1}");
+    for arg in args {
+        if arg.contains("-") {
+            let mut opts: Vec<&str> = arg.split("-").collect();
+            opts.remove(0);
+            
+            let avail_opts = *opts.get(0).unwrap();
+
+            for opt in avail_opts.split("").into_iter() {
+                match opt {
+                    "n" => println!("do not output the trailing newline"),
+                    "e" => println!("enable interpretation of backslash escapes"),
+                    "E" => println!("disable interpretation of backslash escapes (default)"),
+                    _ => continue,
+                }
+            }
+        } else {
+            println!("{arg}");   
+        }
+    }
 }
